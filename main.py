2b7d95bfa4c4a91ad2ac07606535965f5d879c13
fujibo <kt.tsubo@gmail.com>
2017-01-31 21:35:09 +0900

    重み付けありの3x3

--- main.py
+++ main.py
@@ -18,18 +18,37 @@ def winning(board):
         else:
             return False
 
-if __name__ == '__main__':
-    # board 0: blank, 1: white, -1: black
+def game(weights):
     board = np.zeros((N, N), dtype=np.int8)
     turn = True
     end = False
+
     while (board == 0).any():
         # change black and white
         if not turn:
             board = -board
-        # put
+
+        # can move
         places = np.where(board == 0)
-        r = np.random.randint(places[0].size)
+
+        # set algorithm here.
+        nextBoard = []
+        for i in range(places[0].size):
+            tmp = board.copy()
+            tmp[places[0][i], places[1][i]] = 1
+            nextBoard.append(tmp.flatten())
+
+        nextBoard = np.array(nextBoard)
+
+        # 0 0 0 1 -1 1 0 0 0' s array
+        # 0 0 0
+        # 1-1 1
+        # 0 0 0
+        r = np.argmax(weights.dot(nextBoard.transpose()))
+
+        # r = np.random.randint(places[0].size)
+
+        # put
         board[places[0][r], places[1][r]] = 1
 
         # winning state
@@ -53,8 +72,11 @@ if __name__ == '__main__':
         # end of this turn
         turn = not turn
 
-
-
     # all masses are filled.
     else:
         print("draw")
+
+if __name__ == '__main__':
+    # board 0: blank, 1: white, -1: black
+    weights = np.random.rand(1, N*N)
+    game(weights)
